1d3be4ac80810c93d00d1bceabb8012d3cb49f88
anthonymin <dbstnsdl12@naver.com>
2020-11-02 20:15:03 +0900

    setup router and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+import { Global } from './styles';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global />
+      <BrowserRouter>
+        <div>hello</div>
+      </BrowserRouter>
+    </>
   );
 }
 
