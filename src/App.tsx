a0d1e65ff42d73abc1bbd605a076ac07b972e9cd
anthonymin <dbstnsdl12@naver.com>
2020-11-08 14:42:41 +0900

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.css';
 import { Header, List, Nav, SavedList } from './components';
 import { Global } from './styles';
 
